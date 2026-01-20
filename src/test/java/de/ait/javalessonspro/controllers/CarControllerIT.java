b145912039bf221892469651aeafb59c99081417
Alexander Hermann <ewebotah@gmail.com>
2026-01-20 23:47:21 +0100

    refactor(tests): remove unused import in CarControllerIT

--- src/test/java/de/ait/javalessonspro/controllers/CarControllerIT.java
+++ src/test/java/de/ait/javalessonspro/controllers/CarControllerIT.java
@@ -9,7 +9,6 @@ import de.ait.javalessonspro.repository.CarRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.shadow.com.univocity.parsers.common.input.LineSeparatorDetector;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
