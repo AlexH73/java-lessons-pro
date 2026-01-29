367f9c0827457ee1ca56082a9b2674fa478af75f
Alexander Hermann <ewebotah@gmail.com>
2026-01-30 00:23:11 +0100

    chore(EmailController): add controller for sending car offer emails

---
+++ src/main/java/de/ait/javalessonspro/controllers/EmailController.java
@@ -0,0 +1,37 @@
+package de.ait.javalessonspro.controllers;
+
+import de.ait.javalessonspro.dto.CarOfferEmailRequest;
+import de.ait.javalessonspro.service.CarOfferEmailService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+/**
+ * ----------------------------------------------------------------------------
+ * Author  : Alexander Hermann
+ * Created : 29.01.2026
+ * Project : JavaLessonsPro
+ * ----------------------------------------------------------------------------
+ */
+@RestController
+@RequestMapping("/api/email")
+@RequiredArgsConstructor
+@Slf4j
+public class EmailController {
+
+    private final CarOfferEmailService carOfferEmailService;
+
+    @PostMapping("/car-offer")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void sendCarOfferEmail(@RequestBody @Valid CarOfferEmailRequest carOfferEmailRequest) {
+        log.info("Sending car offer email for request: {}", carOfferEmailRequest);
+        carOfferEmailService.sendCarOfferEmail(carOfferEmailRequest);
+    }
+}
