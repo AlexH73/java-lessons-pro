d80a935f6b647959bb941a507bee8ad3f4d18f79
Alexander Hermann <ewebotah@gmail.com>
2026-02-01 03:21:59 +0100

    feat(BookingStatus): introduce BookingStatus enum with initial states

---
+++ src/main/java/de/ait/javalessonspro/enums/BookingStatus.java
@@ -0,0 +1,12 @@
+package de.ait.javalessonspro.enums;
+
+/**
+ * ----------------------------------------------------------------------------
+ * Author  : Alexander Hermann
+ * Created : 01.02.2026
+ * Project : JavaLessonsPro
+ * ----------------------------------------------------------------------------
+ */
+public enum BookingStatus {
+    CONFIRMED, CANCELLED, COMPLETED, REMINDER_SENT
+}
