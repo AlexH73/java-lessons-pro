1244a60c66d1d8ab136fe439c2d2976f5825bfd4
Alexander Hermann <ewebotah@gmail.com>
2026-02-07 10:48:02 +0100

    feat(CandidateDocumentOsRepository): add repository for candidate document operations

---
+++ src/main/java/de/ait/javalessonspro/repositories/CandidateDocumentOsRepository.java
@@ -0,0 +1,18 @@
+package de.ait.javalessonspro.repositories;
+
+import de.ait.javalessonspro.model.CandidateDocumentOs;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * ----------------------------------------------------------------------------
+ * Author  : Alexander Hermann
+ * Created : 07.02.2026
+ * Project : JavaLessonsPro
+ * ----------------------------------------------------------------------------
+ */
+public interface CandidateDocumentOsRepository extends JpaRepository<CandidateDocumentOs, Long> {
+    List<CandidateDocumentOs> findAllByCandidateEmail(String email);
+    void deleteAllByCandidateEmail(String email);
+}
